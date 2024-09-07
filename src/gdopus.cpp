0c640542a232c1c922bfe52a70dbae8797bf1a32
Ben Lubar <ben.lubar@gmail.com>
2024-09-07 14:50:31 -0500

    fix methods having unnamed arguments and (hopefully) fix the wasm build

--- src/gdopus.cpp
+++ src/gdopus.cpp
@@ -6,6 +6,6 @@ using namespace godot;
 
 void Opus::_bind_methods() {
 	// Methods.
-	ClassDB::bind_static_method("Opus", D_METHOD("decode"), &Opus::decode);
-	ClassDB::bind_static_method("Opus", D_METHOD("encode"), &Opus::encode);
+	ClassDB::bind_static_method("Opus", D_METHOD("decode", "data", "audio"), &Opus::decode);
+	ClassDB::bind_static_method("Opus", D_METHOD("encode", "audio"), &Opus::encode);
 }
